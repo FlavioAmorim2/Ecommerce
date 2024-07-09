d787143fc0819ca5620ea0a5313f012758c45274
Flavio <flaviomatheus28013@gmail.com>
2024-07-08 21:57:18 -0300

    research-app, header, Userbuttons. creaet-catItem, CartOverlay, cartproducts, cartContext

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,27 @@
-import logo from '../assets/logo/hashtag.svg';
-import UserButtons from './UserButtons';
-import { useLocation, Link } from 'react-router-dom';
+import logo from "../assets/logo/hashtag.svg";
+import UserButtons from "./UserButtons";
+import { useLocation, Link } from "react-router-dom";
 
 const Header = () => {
-    const { pathname } = useLocation();
-    const completeHeader = (       
-         <header className='flex text-xl sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-6 py-4 items-end justify-between text-base z-10'>
-        <Link to="/">
-            <img className='h-16 px-2' src={logo} alt="Logo Hashtag" />
-        </Link>
-        <UserButtons />
+  const { pathname } = useLocation();
+  const completeHeader = (
+    <header className="flex text-xl sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-6 py-4 items-end justify-between text-base z-10">
+      <Link to="/">
+        <img className="h-16 px-2" src={logo} alt="Logo Hashtag" />
+      </Link>
+      <UserButtons />
     </header>
-    );
+  );
 
-    const simpleHeader = (
-        <header className='flex text-xl sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-6 py-4 items-end justify-between text-base z-10'>
-        <Link to="/">
-            <img className='h-12 px-2' src={logo} alt="Logo Hashtag" />
-        </Link>
+  const simpleHeader = (
+    <header className="flex text-xl sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-6 py-4 items-end justify-between text-base z-10">
+      <Link to="/">
+        <img className="h-12 px-2" src={logo} alt="Logo Hashtag" />
+      </Link>
     </header>
-    );
+  );
 
-    return <> {pathname === '/' ? completeHeader : simpleHeader } </>;
-
-}
+  return <> {pathname === "/" ? completeHeader : simpleHeader} </>;
+};
 
 export default Header;
---
+++ src/context/CartContext.js
@@ -0,0 +1,13 @@
+import { createContext } from "react"
+
+export const CartContext = createContext(null);
+
+export const getAmountOfItemsInCart = (cartItemsObj) => {
+    let amount = 0; 
+
+    for(const productId in cartItemsObj) {
+        amount += cartItemsObj[productId];
+
+    }
+    return amount;
+};
